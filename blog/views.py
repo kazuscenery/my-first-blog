cfa239ec4a3c5bf8e1a8daf65cc0dc75a868873b
kazuscenery <blob2@example.com>
2019-08-20 13:44:00 +0900

    html

--- blog/views.py
+++ blog/views.py
@@ -10,6 +10,7 @@ from django.contrib.auth.decorators import login_required
 
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    #8.20 エラー文内で確認
     return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
